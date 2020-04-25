8eabfe1f8d9b2b87b2970e886d8408db27986631
MartinChiri <martinchiricc07@gmail.com>
2020-04-25 13:25:55 -0500

    Conectados via LiveShare

--- src/main/main.py
+++ src/main/main.py
@@ -2,10 +2,10 @@ from utilitarios import utilitarios_forma as forma
 import pygame
 import numpy as np
 
-pygame.init()
 
-screem = pygame.display.set_mode((forma.alto,forma.ancho))
-screem.fill(forma.bg)
 
-while True:
-    pass
+
+
+
+print(forma.ancho)
+print(forma.alto)
