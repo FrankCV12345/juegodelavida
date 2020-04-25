fea4e58d6f42db5df24fbe8d817e789ef43f8b4f
frank <f.r.a.n.k.c.v.777@gmail.com>
2020-04-25 18:01:16 -0500

    avance martin

--- src/main/main.py
+++ src/main/main.py
@@ -2,10 +2,36 @@ from utilitarios import utilitarios_forma as forma
 import pygame
 import numpy as np
 
+screen = pygame.display.set_mode((forma.alto,forma.ancho))
+screen.fill(forma.bg)
 
+gameState = np.zeros((forma.nxC, forma.nyC))
 
+while True:
+    for y in range(0, forma.nxC) :
+        for x in range(0, forma.nyC) :
 
+            newGameState = np.copy(gameState)
 
+            n_neigh =   gameState[(x - 1) % forma.nxC, (y-1)      % forma.nyC] +    \
+                        gameState[(x)     % forma.nxC, (y-1)      % forma.nyC] +    \
+                        gameState[(x + 1) % forma.nxC, (y-1)      % forma.nyC] +    \
+                        gameState[(x - 1) % forma.nxC, (y)        % forma.nyC] +    \ 
+                        gameState[(x + 1) % forma.nxC, (y)        % forma.nyC] +    \
+                        gameState[(x - 1) % forma.nxC, (y + 1)    % forma.nyC] +    \
+                        gameState[(x)     % forma.nxC, (y + 1)    % forma.nyC] +    \
+                        gameState[(x +1 ) % forma.nxC, (y + 1)    % forma.nyC]
+            
+            if gameState[x, y] == 0 and n_neigh == 3:
+                newGameState[x, y] = 1
+            
+            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                newGameState[x, y] = 0
 
-print(forma.ancho)
-print(forma.alto)
+            poly  = forma.polygono(x,y)
+            
+            if newGameState[x, y]
+
+            pygame.draw.polygon(screen, (128,128,128), poly, 1)
+
+    pygame.display.flip()
--- src/main/utilitarios/utilitarios_forma.py
+++ src/main/utilitarios/utilitarios_forma.py
@@ -1,3 +1,20 @@
 alto = 1000
 ancho = 2000 
 bg = 25,25,25
+
+nxC,nyC = 25,25
+
+dimCW = ancho / nxC
+dimCH = alto / nyC
+
+def polygono(x,y):
+     return  [
+                ((x)        * dimCW, y        * dimCH),
+                ((x + 1)    * dimCW, y        * dimCH),
+                ((x + 1)    * dimCW, (y + 1 ) * dimCH),
+                ((x)        * dimCW, (y + 1 ) * dimCH)  
+            ]
+
+
+
+
